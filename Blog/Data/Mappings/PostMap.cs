d337c3dd67d73de62eaa39d7119106aa3ac9c700
FlpSlva <flpslvadev@gmail.com>
2023-01-30 08:24:55 -0300

    add new imports

--- Blog/Data/Mappings/PostMap.cs
+++ Blog/Data/Mappings/PostMap.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using Blog.Models;
+using Blog.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
