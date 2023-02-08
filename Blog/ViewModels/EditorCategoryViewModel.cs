236173a975ab512f3cd29ba405cf68a7bc79e74e
FlpSlva <flpslvadev@gmail.com>
2023-02-08 16:54:51 -0300

    add new config

--- Blog/ViewModels/EditorCategoryViewModel.cs
+++ Blog/ViewModels/EditorCategoryViewModel.cs
@@ -6,9 +6,9 @@ namespace Blog.ViewModels
     {
         [Required( ErrorMessage = "O nome é obrigatório !")]
         [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter 3 e 40 caracteres")]
-        public string Name { get; set; }
+        public string Name { get; private set; }
 
         [Required(ErrorMessage = "O slug é obrigatório !")]
-        public string Slug { get; set; }
+        public string Slug { get; private set; }
     }
 }
